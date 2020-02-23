9abd9433ab4040c9dbfd9738108af5d18d071120
Gayan Fonseka <gayan@Gayans-MacBook-Pro.local>
2020-02-23 10:52:11 +0530

    Defined Template Layouts

--- webapp/src/main.go
+++ webapp/src/main.go
@@ -3,11 +3,38 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/mgsf/RapidResponseSystem/view"
+	"github.com/mgsf/RapidResponseSystem/wc"
 )
 
 func main() {
 	view.RegisterStaticHandlers()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t, err := view.Get("workcenter")
+		if err != nil {
+			log.Fatal(err)
+		}
+		workcenter := wc.Workcenter{
+			ID:              1,
+			Name:            "Assembly Line 1",
+			CurrentProduct:  "Widgets",
+			Status:          1,
+			EscalationLevel: 3,
+			StatusSetAt:     time.Now(),
+		}
+		err = t.Execute(w, struct {
+			wc.Workcenter
+			view.PipelineBase
+		}{
+			Workcenter:   workcenter,
+			PipelineBase: view.PipelineBase{Title: workcenter.Name},
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
--- webapp/src/view/templates.go
+++ webapp/src/view/templates.go
@@ -1,11 +1,19 @@
 package view
 
 import (
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
+	"os"
 	"path"
+	"strings"
 
 	"github.com/mgsf/RapidResponseSystem/config"
+)
+
+var (
+	templates = map[string]*template.Template{}
 )
 
 // PipelineBase contains fields that are expected to be present for
@@ -33,4 +41,35 @@ func setupViews() {
 	if err != nil {
 		log.Printf("could not parse _header.gohtml or _footer.gohtml: %v", err)
 	}
+	viewFIs, err := ioutil.ReadDir(path.Join(viewRoot, "content"))
+	if err != nil {
+		log.Printf("could not open view content directory: %v", err)
+	}
+	for _, fi := range viewFIs {
+		f, err := os.Open(path.Join(viewRoot, "content", fi.Name()))
+		if err != nil {
+			log.Printf("failed to read content template %q: %v", fi.Name(), err)
+		}
+		content, err := ioutil.ReadAll(f)
+		f.Close()
+		if err != nil {
+			log.Printf("failed to read content from template %q: %v", fi.Name(), err)
+		}
+		tmpl := template.Must(layout.Clone())
+		_, err = tmpl.Parse(string(content))
+		if err != nil {
+			log.Printf("failed to parse template %q: %v", fi.Name(), err)
+		}
+		templates[strings.TrimSuffix(fi.Name(), ".gohtml")] = tmpl
+	}
+}
+
+// Get returns the view template stored with the provided key
+// or an error if no template is found
+func Get(key string) (*template.Template, error) {
+	t, ok := templates[key]
+	if !ok {
+		return nil, fmt.Errorf("cannot find template with key %q", key)
+	}
+	return t, nil
 }
