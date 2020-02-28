b02f48011119e4cc19d1e8d0d8aa3d55898536e6
Gayan Fonseka <gayan@Gayans-MacBook-Pro.local>
2020-02-28 23:38:13 +0530

    Static Routing added

--- webapp/src/main.go
+++ webapp/src/main.go
@@ -11,30 +11,6 @@ import (
 
 func main() {
 	view.RegisterStaticHandlers()
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t, err := view.Get("workcenter")
-		if err != nil {
-			log.Fatal(err)
-		}
-		workcenter := wc.Workcenter{
-			ID:              1,
-			Name:            "Assembly Line 1",
-			CurrentProduct:  "Widgets",
-			Status:          1,
-			EscalationLevel: 3,
-			StatusSetAt:     time.Now(),
-		}
-		err = t.Execute(w, struct {
-			wc.Workcenter
-			view.PipelineBase
-		}{
-			Workcenter:   workcenter,
-			PipelineBase: view.PipelineBase{Title: workcenter.Name},
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
-
+	routes.Register()
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
